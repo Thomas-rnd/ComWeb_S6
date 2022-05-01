d8954b9dcca5f78cc6939ca3eeb1b7e5b06a8fc5
sebleger <sleger003@ensc.fr>
2022-05-01 17:22:14 +0200

    Update histoire.php

--- histoire.php
+++ histoire.php
@@ -2,17 +2,17 @@
 require_once "includes/functions.php";
 session_start();
 
-$movieId = $_GET['id'];
-$stmt = getDb()->prepare('select * from movie where mov_id=?');
-$stmt->execute(array($movieId));
-$movie = $stmt->fetch(); // Access first (and only) result line
+$histId = $_GET['id'];
+$stmt = getDb()->prepare('select * from histoire where HIST_NUM=?');
+$stmt->execute(array($histId));
+$histoire = $stmt->fetch(); // Access first (and only) result line
 ?>
 
 <!doctype html>
 <html>
 
 <?php 
-$pageTitle = $movie['mov_title'];
+$pageTitle = $histoire['HIST_TITRE'];
 require_once "includes/head.php"; 
 ?>
 
@@ -23,12 +23,12 @@ require_once "includes/head.php";
         <div class="jumbotron">
             <div class="row">
                 <div class="col-md-5 col-sm-7">
-                    <img class="img-responsive movieImage" src="images/<?= $movie['mov_image'] ?>" title="<?= $movie['mov_title'] ?>" />
+                    <img class="img-responsive histImage" src="images/<?= $histoire['HIST_IMAGE'] ?>" title="<?= $histoire['HIST_TITRE'] ?>" />
                 </div>
                 <div class="col-md-7 col-sm-5">
-                    <h2><?= $movie['mov_title'] ?></h2>
-                    <p><?= $movie['mov_director'] ?>, <?= $movie['mov_year'] ?></p>
-                    <p><small><?= $movie['mov_description_long'] ?></small></p>
+                    <h2><?= $histoire['HIST_TITRE'] ?></h2>
+                    <p><?= $histoire['HIST_AUTEUR'] ?>, <?= $histoire['HIST_DATE'] ?></p>
+                    <p><small><?= $histoire['HIST_RESUME'] ?></small></p>
                 </h2>
             </div>
         </div>
