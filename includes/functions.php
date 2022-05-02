6fffe9ced72881fcc7314aad2e2578dcc67b7101
Thomas-rnd <renaudthomas@orange.fr>
2022-05-02 13:49:21 +0200

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -3,17 +3,18 @@
 // Connect to the database. Returns a PDO object
 function getDb() {
     // Local deployment
-    /* $server = "localhost";
-    $username = "mymovies_user";
-    $password = "secret";
-    $db = "histoire"; */
+    $server = "localhost";
+    $username = "Admin";
+    $password = "admin";
+    $db = "jesuislehero_renaud_leger"; 
     
+    /*
     // Deployment on Heroku with ClearDB for MySQL
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $server = $url["host"];
     $username = $url["user"];
     $password = $url["pass"];
-    $db = substr($url["path"], 1);
+    $db = substr($url["path"], 1);*/
     
     return new PDO("mysql:host=$server;dbname=$db;charset=utf8", "$username", "$password",
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
