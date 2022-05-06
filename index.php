15036ddcefc009e5315bab6c099aaf8958fe5682
Thomas-rnd <renaudthomas@orange.fr>
2022-05-06 09:56:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,15 +14,9 @@ $histoires = getDb()->query('select * from histoire order by HIST_NUM desc');
 <body>
     <div class="container">
         <?php require_once "includes/header.php"; ?>
-<<<<<<< Updated upstream
-<<<<<<< HEAD
             <div class="container">
-=======
         <div class="d-flex">
->>>>>>> 9cbe5534a931b425f4afa167d0b84ef8fb5c2b78
-=======
         <div class="container">
->>>>>>> Stashed changes
         <?php foreach ($histoires as $histoire) { ?>
             <article>
                 <h3><a class="titreHistoire" href="histoire.php?id=<?=$histoire['HIST_NUM']?>"><?=$histoire['HIST_TITRE']?></a></h3>
