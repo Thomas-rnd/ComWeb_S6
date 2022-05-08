6edbea21ee64ffe9f40b9037c50a06d96bc53cc1
Thomas-rnd <renaudthomas@orange.fr>
2022-05-08 17:44:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,10 +18,10 @@ $histoires = getDb()->query('select * from histoire order by HIST_NUM desc');
 
 <body>
     <div class="container">
-        <div class= "page-header">
         <?php require_once "includes/header.php"; ?>
-        </div>
             <div class="container">
+        <div class="d-flex">
+        <div class="container">
         <?php foreach ($histoires as $histoire) { ?>
             <article>
                 <h3><a class="titreHistoire" href="histoire.php?id=<?=$histoire['HIST_NUM']?>"><?=$histoire['HIST_TITRE']?></a></h3>
