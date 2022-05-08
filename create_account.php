f9305ca0dae2d062ab98dbefd308b9607ffaf4c8
Thomas-rnd <renaudthomas@orange.fr>
2022-05-08 18:12:23 +0200

    Create create_account.php

---
+++ create_account.php
@@ -0,0 +1,71 @@
+<?php
+require_once "includes/functions.php";
+session_start();
+
+if (!empty($_POST['login']) and !empty($_POST['password'])) {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $stmt = getDb()->prepare('select * from user where USR_LOGIN=?');
+    $stmt->execute(array($login));
+    if ($stmt->rowCount() == 0) {
+        // Login not already used
+        $stmt = getDb()->prepare('insert into user
+          (USR_LOGIN, USR_PASSWORD)
+          values (?, ?)');
+          $stmt->execute(array($login, $password));
+          redirect("index.php");
+    }
+    else {
+        $error = "Identifiant déjà utilisé";
+    }
+}
+?>
+
+<!doctype html>
+<html>
+
+<?php 
+$pageTitle = "Inscription";
+require_once "includes/head.php";
+?>
+
+<body>
+    <div class="container">
+        <?php require_once "includes/header.php"; ?>
+
+        <h2 class="text-center"><?= $pageTitle ?></h2>
+
+        <?php if (isset($error)) { ?>
+            <div class="alert alert-danger">
+                <strong>Erreur !</strong> <?= $error ?>
+            </div>
+        <?php } ?>
+
+        <div class="well">
+            <form class="form-signin form-horizontal" role="form" action="create_account.php" method="post">
+                <div class="form-group">
+                    <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                    <input type="text" name="login" class="form-control" placeholder="Entrez votre login" required autofocus>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                        <input type="password" name="password" class="form-control" placeholder="Entrez votre mot de passe" required>
+                    </div>
+                </div>
+                <p class="message text-center">Déjà un compte ? <a href="login.php">Se connecter</a></p>
+                <div class="form-group">
+                    <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                        <button type="submit" class="btn btn-default btn-primary"><span class="glyphicon glyphicon-log-in"></span> S'inscrire</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <?php require_once "includes/footer.php"; ?>
+    </div>
+
+    <?php require_once "includes/scripts.php"; ?>
+</body>
+
+</html>
