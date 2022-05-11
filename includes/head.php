bdad24c2783a8416a2b286221b50baf4a3e25bb7
Thomas-rnd <renaudthomas@orange.fr>
2022-05-11 22:43:18 +0200

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -3,6 +3,5 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
     <title>RENAUD LEGER <?php if (isset($pageTitle)) echo "- $pageTitle"; ?></title>
 </head>
