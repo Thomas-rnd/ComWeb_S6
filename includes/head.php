e5ddc67fd2c616fdd0dd8fc18d541a27c69dff66
Thomas-rnd <renaudthomas@orange.fr>
2022-05-12 16:45:33 +0200

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,29 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Site crée par 2 élèves de l'ENSC ayant pour but de mettre à disposition des histoires dont vous êtes le héros">
+    <meta name="author" content="LEGER Sébastien et RENAUD Thomas">
+    <meta name="docsearch:language" content="fr">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/components/navbar/">
+    <!-- Bootstrap core CSS -->
+    <link href="/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Documentation extras -->
+    <link href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" rel="stylesheet">
+    <link href="/docs/4.3/assets/css/docs.min.css" rel="stylesheet">
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" rel="stylesheet">
+
+    <script defer src="https://cdn.usefathom.com/script.js" data-site="ITUSEYJG"></script>
+    <script>
+        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+        ga('create', 'UA-146052-10', 'getbootstrap.com');
+        ga('set', 'anonymizeIp', true);
+        ga('send', 'pageview');
+    </script>
+    <script async src="https://www.google-analytics.com/analytics.js"></script>
+
     <title>RENAUD LEGER <?php if (isset($pageTitle)) echo "- $pageTitle"; ?></title>
 </head>
