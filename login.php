6eaa70177139289e0c10a1723a805711e6dd23b6
Thomas-rnd <renaudthomas@orange.fr>
2022-05-17 15:03:27 +0200

    Update login.php

--- login.php
+++ login.php
@@ -2,17 +2,20 @@
 require_once "includes/functions.php";
 session_start();
 
-if (!empty($_POST['login']) and !empty($_POST['password'])) {
+if (!empty($_POST['login']) and !empty($_POST['password'])) 
+{
     $login = $_POST['login'];
     $password = $_POST['password'];
     $stmt = getDb()->prepare('select * from user where USR_LOGIN=? and USR_PASSWORD=?');
     $stmt->execute(array($login, $password));
-    if ($stmt->rowCount() == 1) {
+    if ($stmt->rowCount() == 1) 
+    {
         // Authentication successful
         $_SESSION['login'] = $login;
         redirect("index.php");
     }
-    else {
+    else 
+    {
         $error = "Utilisateur non reconnu";
     }
 }
@@ -27,42 +30,37 @@ require_once "includes/head.php";
 ?>
 
 <body>
+    <?php require_once "includes/header.php"; ?>
     <div class="container">
-        <?php require_once "includes/header.php"; ?>
-
         <h2 class="text-center"><?= $pageTitle ?></h2>
-
-        <?php if (isset($error)) { ?>
+        <?php if (isset($error)) 
+        { ?>
             <div class="alert alert-danger">
                 <strong>Erreur !</strong> <?= $error ?>
             </div>
         <?php } ?>
-
         <div class="well">
             <form class="form-signin form-horizontal" role="form" action="login.php" method="post">
                 <div class="form-group">
-                    <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-                    <input type="text" name="login" class="form-control" placeholder="Entrez votre login" required autofocus>
+                    <div class="col-sm col-sm-offset col-md col-md-offset">
+                        <input type="text" name="login" class="form-control" placeholder="Entrez votre login" required autofocus>
                     </div>
                 </div>
                 <div class="form-group">
-                    <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                    <div class="d-grid gap-2">
                         <input type="password" name="password" class="form-control" placeholder="Entrez votre mot de passe" required>
                     </div>
                 </div>
                 <p class="message text-center">Pas de compte ? <a href="create_account.php">Se créer un compte</a></p>
                 <div class="form-group">
-                    <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-                        <button type="submit" class="btn btn-default btn-primary"><span class="glyphicon glyphicon-log-in"></span> Se connecter</button>
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn btn-default btn-primary mx-auto"><span class="glyphicon glyphicon-log-in"></span> Se connecter</button>
                     </div>
                 </div>
             </form>
-        </div>
-
-        <?php require_once "includes/footer.php"; ?>
+        </div> 
     </div>
-
+    <?php require_once "includes/footer.php"; ?>
     <?php require_once "includes/scripts.php"; ?>
 </body>
-
 </html>
