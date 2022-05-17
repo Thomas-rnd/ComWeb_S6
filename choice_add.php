2604c13ab68f3793cb1bddac49a31efa499c97d3
Thomas-rnd <renaudthomas@orange.fr>
2022-05-17 15:02:35 +0200

    Update choice_add.php

--- choice_add.php
+++ choice_add.php
@@ -2,8 +2,10 @@
 require_once "includes/functions.php";
 session_start();
 
-if (isAdminConnected()) {
-    if (isset($_POST['choix'])) {
+if (isAdminConnected()) 
+{
+    if (isset($_POST['choix'])) 
+    {
         for($i=0;$i<count($_POST['choix']);$i++)
         {
             $choix = escape($_POST['choix'][$i]);  
@@ -16,49 +18,48 @@ if (isAdminConnected()) {
             $stmt->execute(array($choix, $indexChoix, $_SESSION['narrId'], $_SESSION['histId']));
         }
         redirect("index.php");
-    }}
-    ?>
+    }
+}?>
 
-  <!doctype html>
-  <html>
+<!doctype html>
+<html>
 
-  <?php
-    $pageTitle = "Ajout d'une histoire";
-    require_once "includes/head.php";
-    ?>
+<?php
+$pageTitle = "Ajout d'une histoire";
+require_once "includes/head.php";
+?>
 
-    <body>
-        <?php require_once "includes/header.php"; ?>
+<body>
+    <?php require_once "includes/header.php"; ?>
+    <div class="container">
         <h2 class="text-center">Ajout d'un choix</h2>
-            <div class="container">
-            <div class="well">
-                <form class="form-horizontal" role="form" enctype="multipart/form-data" action="choice_add.php" method="post">
-                    <?php for($i=0;$i<$_SESSION['nbChoix'];$i++){?>
-                        <div class="form-group">
-                            <label class="col-sm-auto">Choix : </label>
-                            <div class="col-sm-6">
-                                <textarea name="choix[]" class="form-control" rows="3" placeholder="Définition du choix" required></textarea>
-                            </div>
+        <div class="well">
+            <form class="form-horizontal" role="form" enctype="multipart/form-data" action="choice_add.php" method="post">
+                <?php for($i=0;$i<$_SESSION['nbChoix'];$i++)
+                {?>
+                    <div class="form-group">
+                        <label class="col-sm-auto">Choix : </label>
+                        <div class="col-sm-6">
+                            <textarea name="choix[]" class="form-control" rows="3" placeholder="Définition du choix" required></textarea>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleSelect1" class="form-label mt-4">Index du choix : </label>
-                            <div class="col-sm-6">
-                                <input type="number" name="indexChoix[]" class="form-control" placeholder="Entrez l'index de retour du choix" min="0" required>
-                            </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleSelect1" class="form-label mt-4">Index du choix : </label>
+                        <div class="col-sm-6">
+                            <input type="number" name="indexChoix[]" class="form-control" placeholder="Entrez l'index de retour du choix" min="0" required>
                         </div>
-                    <?php } ?>
-                        <div class="form-group">
-                            <div class="col-sm-4 col-sm-offset-4">
-                                <button type="submit" class="btn btn-default btn-primary"><span class="glyphicon glyphicon-save"></span> Enregistrer</button>
-                            </div>
-                        </div>
-                </form>
-          </div>
-
-          <?php require_once "includes/footer.php"; ?>
-      </div>
-
-      <?php require_once "includes/scripts.php"; ?>
-    </body>
-
-  </html>
+                    </div>
+                <?php } ?>
+                </br></br>
+                <div class="form-group">
+                    <div class="col-sm-4 col-sm-offset-4">
+                        <button type="submit" class="btn btn-default btn-primary"><span class="glyphicon glyphicon-save"></span> Enregistrer</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php require_once "includes/footer.php"; ?>
+<?php require_once "includes/scripts.php"; ?>
+</body>
+</html>
