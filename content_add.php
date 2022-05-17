38a5fa8cb43b1b60548f3048b9d241b7a76e79bf
Thomas-rnd <renaudthomas@orange.fr>
2022-05-17 15:02:47 +0200

    Update content_add.php

--- content_add.php
+++ content_add.php
@@ -2,11 +2,12 @@
 require_once "includes/functions.php";
 session_start();
 
-if (isAdminConnected()) {
-
+if (isAdminConnected()) 
+{
     $histoires = getDb()->query('select * from histoire order by HIST_NUM desc'); 
     
-    if (isset($_POST['narration'])) {
+    if (isset($_POST['narration'])) 
+    {
         // the history form has been posted : retrieve movie parameters
         $histoire = escape($_POST['histoire']);
         $stmt = getDb()->prepare('select * from histoire where HIST_TITRE=?');
@@ -31,37 +32,45 @@ if (isAdminConnected()) {
         $_SESSION['nbChoix']=$nbChoix;
 
         $narrNonCrées = getDb()->prepare('select * from choix where NARR_INDEX!=ALL(select NARR_INDEX from narration)');
-        $narrNonCrées->execute();  
-        redirect("choice_add.php");
-    }}
-    ?>
+        $narrNonCrées->execute();
+        if($nbChoix!=0)
+        {
+            redirect("choice_add.php");
+        }
+        else
+        {
+            redirect("index.php");
+        }
+    }
+}?>
 
-  <!doctype html>
-  <html>
+<!doctype html>
+<html>
 
-  <?php
-    $pageTitle = "Ajout d'une histoire";
-    require_once "includes/head.php";
-    ?>
+<?php
+$pageTitle = "Ajout d'une narration";
+require_once "includes/head.php";
+?>
 
-    <body>
+<body>
+    <?php require_once "includes/header.php"; ?>
         <div class="container">
-            <?php require_once "includes/header.php"; ?>
             <h2 class="text-center">Ajout du contenu</h2>
             <div class="container">
                 <div class="well">
                     <form class="form-horizontal" role="form" enctype="multipart/form-data" action="content_add.php" method="post">
                         <div class="form-group">
                             <label for="exampleSelect1" class="form-label mt-4">Sélection d'une histoire : </label>
-                                <select name="histoire" class="form-select" id="exampleSelect1">
-                                    <?php while($numHist = $histoires->fetch()) 
-                                    { ?>
-                                        <div class="col-sm">
-                                            <option><?=$numHist['HIST_TITRE']?></option>
-                                        </div>
-                                    <?php } ?>
-                                </select>
+                            <select name="histoire" class="form-select" id="exampleSelect1">
+                                <?php while($numHist = $histoires->fetch()) 
+                                {?>
+                                    <div class="col-sm">
+                                        <option><?=$numHist['HIST_TITRE']?></option>
+                                    </div>
+                                <?php } ?>
+                            </select>
                         </div>
+                        </br>
                         <!--
                         <div class="form-group">
                             <label for="exampleSelect1" class="form-label mt-4">Les narrations à créer : </label>
@@ -81,6 +90,7 @@ if (isAdminConnected()) {
                                 <textarea name="narration" class="form-control" rows="3" placeholder="Entrez du texte de narration" required></textarea>
                             </div>
                         </div>
+                        </br>
                         <div class="form-group">
                                 <label for="exampleSelect1" class="form-label mt-4">Nombre de choix possible : </label>
                                 <select name="nbChoix" class="form-select" id="exampleSelect1">
@@ -90,16 +100,17 @@ if (isAdminConnected()) {
                                     <option>3</option>
                                 </select>
                         </div>
+                        </br></br>
                         <div class="form-group">
-                            <div class="col-sm-4 col-sm-offset-4">
-                                <button type="submit" class="btn btn-default btn-primary"><span class="glyphicon glyphicon-save"></span> Enregistrer</button>
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn btn-default btn-primary mx-auto"><span class="glyphicon glyphicon-save"></span> Enregistrer</button>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
-            <?php require_once "includes/footer.php"; ?>
         </div>
+        <?php require_once "includes/footer.php"; ?>
         <?php require_once "includes/scripts.php"; ?>
     </body>
 
